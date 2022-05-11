f3b46f40e8bb1726ee908b42eb8b16547aa434b2
Eduardo Ribeiro <95627481+ribeiroedu88@users.noreply.github.com>
2022-05-11 16:53:46 -0300

    alt

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 ////////////////////CONFIGURAÇÃO DO MENU MOBILE/////////////////////
 
-const btnMobile = document.getElementById('btn-mobile')
+const btnMobile = document.getElementById('btn-mobile');
 
     function toggleMenu() {
-        const nav = document.getElementById('nav')
-        nav.classList.toggle('active')
+        const nav = document.getElementById('nav-bar');
+        nav.classList.toggle('active');
     }
 
-btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('click', toggleMenu);
 
 
-document.querySelector("#carrossel-items")
-.addEventListener("wheel", event => {
-    if(event.deltaY > 0) {
-        event.target.scrollBy (300, 0)
-    } else {
-        event.target.scrollBy (-300, 0)
-    }
-})
+// document.querySelector("#carrossel-items")
+// .addEventListener("wheel", event => {
+//     if(event.deltaY > 0) {
+//         event.target.scrollBy (300, 0)
+//     } else {
+//         event.target.scrollBy (-300, 0)
+//     }
+// })
