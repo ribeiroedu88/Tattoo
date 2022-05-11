db656cc0ce055229c96d7136e73835b4034b8295
Eduardo Ribeiro <95627481+ribeiroedu88@users.noreply.github.com>
2022-05-11 17:41:25 -0300

    alt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 ////////////////////CONFIGURAÇÃO DO MENU MOBILE/////////////////////
 
+const hamburgerMenu = document.querySelector('.hamburger'); //Seleciona o elemento com o nome da classe hamburger.
+    //Criando uma função de seta para alterar a classe para ativa.
+    const menuIsActive = () => {
+      //Selecionando o menu hambúrguer e adicionando e removendo a classe de ativo ao clicar.
+      hamburgerMenu.classList.toggle('active');
+    };
+    //Adiciona um ouvinte de evento de 'clique' ao menu de hambúrguer. Em seguida, passamos a função que criamos onde a classe é ativada e desativada.
+    hamburgerMenu.addEventListener('click', menuIsActive);
+    
+
 const btnMobile = document.getElementById('btn-mobile');
 
     function toggleMenu() {
@@ -8,7 +18,6 @@ const btnMobile = document.getElementById('btn-mobile');
     }
 
 btnMobile.addEventListener('click', toggleMenu);
-
 
 // document.querySelector("#carrossel-items")
 // .addEventListener("wheel", event => {
