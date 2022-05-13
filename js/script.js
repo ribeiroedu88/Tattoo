205c89a9795e79d6f31ac294e758c6b3a054912f
Eduardo Ribeiro <95627481+ribeiroedu88@users.noreply.github.com>
2022-05-12 21:44:27 -0300

    alt

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 
 const _elements = {
 	scrollLinks: document.querySelectorAll(".menu-items"),
-    navbarList: document.querySelector(".menu-list")
 }
 
 _elements.scrollLinks.forEach(link => {
     link.addEventListener("click", e => {
-        _elements.navbarList.classList.remove("#nav-bar__menu");
 
         const id = link.getAttribute("href");
         const element = document.querySelector(id);
@@ -35,7 +33,7 @@ const hamburgerMenu = document.querySelector('.hamburger'); //Seleciona o elemen
     //Adiciona um ouvinte de evento de 'clique' ao menu de hambúrguer. Em seguida, passamos a função que criamos onde a classe é ativada e desativada.
     hamburgerMenu.addEventListener('click', menuIsActive);
     
-
+////////////////////CONFIGURAÇÃO DO MENU LISTA/////////////////////
 const btnMobile = document.getElementById('btn-mobile');
 
     function toggleMenu() {
@@ -44,6 +42,15 @@ const btnMobile = document.getElementById('btn-mobile');
     }
 
 btnMobile.addEventListener('click', toggleMenu);
+
+const menuLista = document.getElementById('nav-bar__menu');
+
+    function toggleLista() {
+        const lista = document.getElementById('nav-bar');
+        lista.classList.toggle('active');
+    }
+
+    menuLista.addEventListener('click', toggleLista);
 
 
 
